cf04d8a491dda11e2e24f3000a2220c2aa11a9e7
Plogging Dev <ploggingdev@gmail.com>
2016-11-27 22:40:23 -0800

    Floating point issues

---
+++ funny_floats.py
@@ -0,0 +1,49 @@
+from decimal import getcontext, Decimal
+from math import pi
+from fractions import Fraction
+
+print("0.1 + 0.2 = {}".format(0.1+0.2))
+
+print("0.1 + 0.4 = {}".format(0.1+0.4))
+
+print("1/10 = {}".format(1/10))
+
+print("Is (0.1 + 0.1 + 0.1 == 0.3) ?")
+
+print(0.1 + 0.1 + 0.1 == 0.3)
+
+def safe_add(a,b):
+    return round(a + b)
+
+print("Is (0.1 + 0.1 + 0.1 == 0.3) ?")
+
+print(round(0.111111 + 0.111111 + 0.111111, 5) == round(0.333333, 5))
+
+print(round(10.33333, 3))
+
+#Decimal
+
+print(getcontext())
+
+getcontext().prec = 5
+
+print(Decimal(1)/Decimal(3))
+
+getcontext().prec = 30
+
+print(Decimal(pi))
+
+#Fraction
+
+num1 = Fraction(2,3)
+num2 = Fraction(1,3)
+
+print("num1 = {} and num2 = {}".format(num1,num2))
+
+print(num1 + num2)
+
+print(num1 - num2)
+
+print(num1*10)
+
+print(num1/num2)
